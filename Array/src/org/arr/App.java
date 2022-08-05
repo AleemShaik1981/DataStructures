de5dc112acaf0ffdca63f7fc427a2cd75fcc3d89
Dell G15 <Dell G15@DESKTOP-MAQ19GB>
2022-08-05 20:01:20 +0530

    This is the first commit of array 05-08-2022

---
+++ Array/src/org/arr/App.java
@@ -0,0 +1,13 @@
+package org.arr;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		String[] strArrya = {"one", "two","three"}; 
+		for(String str: strArrya) {
+			System.out.println(str);
+		}
+	}
+
+}
